cc8247178f307ad845b62fe45b1b19462e77427d
jypark <asa855@nate.com>
2024-09-04 19:44:30 +0900

    [feat] init

---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>hello</button>;
+};
+
+export default Button;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("app") as HTMLElement;
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
