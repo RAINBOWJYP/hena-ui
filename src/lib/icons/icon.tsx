390179f802a72b3cc51e77aa1c887ffefcc95c5f
RAINBOWJYP <asa855@nate.com>
2024-10-09 15:39:55 +0900

    [feat] Button 컴포넌트 작성
    * 아이콘 유무 지원
    * 컬러, 타입별 버튼 지원

---
+++ src/lib/icons/icon.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import * as Icon from "./_icon";
+
+export interface IconProps {
+  width?: number;
+  height?: number;
+  onClick?: () => void;
+  iconName: keyof typeof Icon;
+  className?: string;
+}
+
+const HenaIcon = ({
+  width,
+  height,
+  onClick,
+  iconName,
+  className,
+}: IconProps) => {
+  const IconElement = Icon[iconName];
+
+  return (
+    <IconElement
+      width={width}
+      height={height}
+      onClick={onClick}
+      className={className}
+    />
+  );
+};
+
+export default HenaIcon;
